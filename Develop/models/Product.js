2b7dd854118541ea6269a9033ddee357a428e04a
MAS Hargrave <riverbendwarriors13@gmail.com>
2023-05-04 19:59:32 -0400

    wrote models for seeds.

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,25 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+          model: 'Category',
+          key: 'id'
+      },
+    }
   },
   {
     sequelize,
